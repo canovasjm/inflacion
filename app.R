c27c91f9d1845908c85af363f1742b2716fcb174
canovasjm <juanmacanovas@gmail.com>
2023-05-28 21:52:24 -0300

    Add monthly inflation plot

--- app.R
+++ app.R
@@ -56,8 +56,11 @@ ui <- fluidPage(
     
     # Display plot and table
     mainPanel(
-      h4("Inflación acumulada a traves del tiempo"),
-      plotOutput("inflation_plot"),
+      h4("Inflación a través del tiempo"),
+      fluidRow(
+        column(width = 6, plotOutput("inflation_plot1")), # First plot
+        column(width = 6, plotOutput("inflation_plot2"))  # Second plot
+      ),
       hr(),
       h4("Inflación acumulada - Valores"),
       tableOutput("inflation_table")
@@ -83,20 +86,35 @@ server <- function(input, output) {
     paste0(round(cumulative_inflation() * 100, 2), "%")
   })
   
-  # Create plot
-  output$inflation_plot <- renderPlot({
+  # Create plot 1
+  output$inflation_plot1 <- renderPlot({
+    start_date <- as.Date(paste0(input$start_date, "-01"))
+    end_date <- as.Date(paste0(input$end_date, "-01"))
+    months_data <- inflation_data[inflation_data$date >= start_date & inflation_data$date <= end_date, ]
+    months_data$month_year <- format(as.Date(months_data$date), "%Y-%m")
+    ggplot(months_data, aes(x = month_year, y = months_data$inflation)) +
+      geom_col(linewidth = 1, group = 1) +
+      scale_y_continuous(labels = function(x) paste0(x, "%")) +
+      theme(axis.text.x = element_text(angle = 90)) +
+      labs(#title = "Cumulative Inflation Over Time",
+        x = "Año-mes",
+        y = "Inflación mensual")
+  })
+  
+  # Create plot 2
+  output$inflation_plot2 <- renderPlot({
     start_date <- as.Date(paste0(input$start_date, "-01"))
     end_date <- as.Date(paste0(input$end_date, "-01"))
     months_data <- inflation_data[inflation_data$date >= start_date & inflation_data$date <= end_date, ]
     months_data$month_year <- format(as.Date(months_data$date), "%Y-%m")
     months_data$cumulative_inflation <- cumprod(1 + (as.numeric(months_data$inflation) / 100)) - 1
     ggplot(months_data, aes(x = month_year, y = cumulative_inflation)) +
-      geom_col(linewidth = 1, group = 1) +
+      geom_line(linewidth = 1, group = 1) +
       scale_y_continuous(labels = scales::percent) +
       theme(axis.text.x = element_text(angle = 90)) +
       labs(#title = "Cumulative Inflation Over Time",
-           x = "Año-mes",
-           y = "Inflación acumulada")
+        x = "Año-mes",
+        y = "Inflación acumulada")
   })
 
   # Create table
